d27397e70b76792d2e56071140161ea1e19fb4f7
Sudhanshu Passi <sudhanshupassi@dijkstra.local>
2019-03-01 21:09:40 +0530

    bugfix

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ class Index(object):
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         resp.content_type = 'text/html'
-        with open('../frontend/build/index.html', 'r') as f:
+        with open('./frontend/build/index.html', 'r') as f:
             resp.body = f.read()
 
 
